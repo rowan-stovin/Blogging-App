ad656915932e1ee0e6ebdb05d2ce51252fcb862a
Elliot Simpson <elliotsimpson@uvic.ca>
2025-10-29 14:53:32 -0700

    Implement eq method in blog.py

--- blogging/blog.py
+++ blogging/blog.py
@@ -4,3 +4,9 @@ class Blog:
         self.name = name
         self. url = url
         self.email = email
+
+    def __eq__(self, other) -> bool:
+        return self.id == other.id \
+            and self.name == other.name \
+            and self.url == other.url \
+            and self.email == other.email
