20c6ed195e14890f84ecca520c556b5eafe8bf95
Elliot Simpson <elliotsimpson@uvic.ca>
2025-10-29 12:51:16 -0700

    Update blog.py methods

--- blogging/controller.py
+++ blogging/controller.py
@@ -3,49 +3,52 @@ from post import Post
 
 class Controller:
     def __init__(self):
-        self.login_flag = False
+        self.logged_in = False
         self.username = None
         self.password = None
+        self.blog_collection = {}
     
-    def set_username(self, username: str):
+    def set_username(self, username: str) -> None:
         self.username = username
 
-    def set_password(self, password: int):
+    def set_password(self, password: int) -> None:
         self.password = password
 
-    def logout(self):
-        if not self.login_flag:
+    def logout(self) -> bool:
+        if not self.logged_in:
             return False
-        
+
         self.set_password = None
         self.set_username = None
+        self.logged_in = False
 
         return True
-    
-    def login(self, username: str, password: str):
-        if self.login_flag:
+
+    def login(self, username: str, password: str) -> bool:
+        # Can't login if already logged in
+        if self.logged_in:
             return False
-        elif not username == self.username:
+        elif self.username != username or self.password != password:
             return False
-        elif not password == self.password:
-            return False
-        else:
-            return True
-        
-    def create_blog(self, id: int, name: str, url: str, email: str):
-        if not self.login_flag:
+
+        self.logged_in = True
+        return True
+
+    def create_blog(self, id: int, name: str, url: str, email: str) -> Blog:
+        # If user isn't logged in, or blog already exists
+        if not self.logged_in or self.blog_collection[id]:
             return None
-        
+
         self.blog_collection[id] = Blog(id, name, url, email)
         return self.blog_collection[id]
 
-    def search_blog(self, id: int):
+    def search_blog(self, id: int) -> Blog:
         return self.blog_collection[id]
-        
+
     def update_blog(self, old_id: int, new_id: int, name: str, url: str, email: str):
         if old_id == new_id:
             return self.blog_collection[old_id]
-        
+
         self.blog_collection[new_id] = self.create_blog(new_id, name, url, email)
         self.blog_collection[old_id] = None
 
