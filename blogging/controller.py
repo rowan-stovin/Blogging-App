5101049be25d3c22549c84f5d6653d5fa14648be
Elliot Simpson <elliotsimpson@uvic.ca>
2025-10-29 16:16:45 -0700

    More misc method changes in controller.py

--- blogging/controller.py
+++ blogging/controller.py
@@ -45,22 +45,33 @@ class Controller:
     def search_blog(self, id: int) -> Blog:
         return self.blog_collection.get(id)
 
-    def update_blog(self, old_id: int, new_id: int, name: str, url: str, email: str):
-        if old_id == new_id:
-            return self.blog_collection.get(old_id)
+    def update_blog(self, old_id: int, new_id: int, name: str, url: str, email: str) -> bool:
+        # We don't want to update to an existing id, unless we are modifying a blog and keeping same id.
+        if self.blog_collection.get(new_id) and old_id != new_id:
+            return False
 
+        # We update values even if id is same
         self.blog_collection[new_id] = self.create_blog(new_id, name, url, email)
-        self.blog_collection[old_id] = None
 
-        return self.blog_collection[new_id]
+        # Delete old (if exists) if not same as new (because it hasn't been overridden)
+        if old_id != new_id and self.blog_collection.get(old_id):
+            del self.blog_collection[old_id]
+
+        return True
 
     def retrieve_blogs(self, keyword) -> list[Blog]:
-        blogs = []
         if not self.logged_in:
             return
 
+        blogs = []
         for blog in self.blog_collection.values():
             if keyword in blog.name:
                 blogs.append(blog)
 
         return blogs
+    
+    def list_blogs(self) -> list[Blog]:
+        if not self.logged_in:
+            return
+        
+        return [blog for blog in self.blog_collection.values()]
