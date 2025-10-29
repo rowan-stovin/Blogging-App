8da939489a7badbd3b965ac029e7599e57bfec36
Rowan Stovin <rowanstovin@uvic.ca>
2025-10-28 17:25:26 -0700

    added basic controller file functions

---
+++ blogging/controller.py
@@ -0,0 +1,21 @@
+from blog import Blog
+from post import Post
+
+class Controller:
+    def __init__(self):
+        self.login_flag = False
+        self.username = ""
+        self.password = ""
+    
+    def logout(self):
+        return self.login_flag
+    
+    def login(self, username, password):
+        if(self.login_flag):
+            return "cannot login again while still logged in"
+        elif(not username.equals(self.username)):
+            return "login in with incorrect username"
+        elif(not password.equals(self.password)):
+            return "login in with incorrect password"
+        
+        
