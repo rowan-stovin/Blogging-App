5bc6b1f0f895fda30ac4ed071fa1bcec868ac7b4
Elliot Simpson <elliotsimpson@uvic.ca>
2025-10-31 15:23:21 -0700

    Add eq method for Post in post.py

--- blogging/post.py
+++ blogging/post.py
@@ -7,3 +7,10 @@ class Post:
         self.text = text
         self.creation = datetime.now()
         self.update = None
+
+    def __eq__(self, other):
+        # I don't think we need to compare datatime's
+        # From what it says in test file.
+        return self.code == other.code /
+            and self.title == other.title /
+            and self.text == other.text
