99d85565190b2f6f963ceb0cb1f2057be50d48be
Elliot Simpson <elliotsimpson@uvic.ca>
2025-11-19 17:24:18 -0800

    Implemented necessary CRUD operations in post_dao_pickle.py per post_dao.py, refactored blog.py to utilize these methods.

--- blogging/blog.py
+++ blogging/blog.py
@@ -1,5 +1,6 @@
 import datetime
 from blogging.post import Post
+from blogging.dao.post_dao_pickle import PostDAOPickle
 
 class Blog():
 	''' class that represents a blog '''
@@ -7,8 +8,8 @@ class Blog():
 	def __init__(self, id, name, url, email):
 		''' construct a blog '''
 		self.counter = 0
-		self.posts = []
-
+		# self.posts = [] # replacing with collection inside of PostDAOPickle object
+		self.post_dao_pickle = PostDAOPickle(self)	
 		self.id = id
 		self.name = name
 		self.url = url
@@ -26,71 +27,29 @@ class Blog():
 		''' converts the blog object to a string representation for debugging '''
 		return "Blog(%r, %r, %r, %r)" % (self.id, self.name, self.url, self.email)
 
-	def search_post(self, code):
+	def search_post(self, code) -> Post:
 		''' search a post in the blog '''
-		for post in self.posts:
-			if post.code == code:
-				return post
-		return None
+		return self.post_dao_pickle.search_post(code)
 
-	def create_post(self, title, text):
+	def create_post(self, title, text) -> Post:
 		''' create a post in the blog '''
 		self.counter += 1
 		new_post = Post(self.counter, title, text)
-		self.posts.append(new_post)
-		return new_post
+		return self.post_dao_pickle.create_post(new_post)
 
-	def retrieve_posts(self, search_string):
+	def retrieve_posts(self, search_string) -> list[Post]:
 		''' retrieve posts in the blog that satisfy a search string '''
-		# retrieve existing posts
-		retrieved_posts = []
-		for post in self.posts:
-			if search_string in post.title or search_string in post.text:
-				retrieved_posts.append(post)
-		return retrieved_posts
+		return self.post_dao_pickle.retrieve_posts(search_string)
 
-	def update_post(self, code, new_title, new_text):
+	def update_post(self, code, new_title, new_text) -> bool:
 		''' update a post from the blog '''
-		updated_post = None
+		return self.post_dao_pickle.update_post(code, new_title, new_text)
 
-		# first, search the post by code
-		for post in self.posts:
-			if post.code == code:
-				updated_post = post
-				break
-
-		# post does not exist
-		if not updated_post:
-			return False
-
-		# post exists, update fields and update timestamp
-		updated_post.update(new_title, new_text)
-		return True
-
-	def delete_post(self, code):
+	def delete_post(self, code) -> bool:
 		''' delete a post from the blog '''
-		post_to_delete_index = -1
+		return self.post_dao_pickle.delete_post(code)
 
-		# first, search the post by code
-		for i in range(len(self.posts)):
-			if self.posts[i].code == code:
-				post_to_delete_index = i
-				break
-
-		# post does not exist
-		if post_to_delete_index == -1:
-			return False
-
-		# post exists, delete post
-		self.posts.pop(post_to_delete_index)
-		return True
-
-	def list_posts(self):
+	def list_posts(self) -> list[Post]:
 		''' list all posts from the blog from the 
 			more recently added to the least recently added'''
-
-		# list existing posts
-		posts_list = []
-		for i in range(-1, -len(self.posts)-1, -1):
-			posts_list.append(self.posts[i])
-		return posts_list
+		return self.post_dao_pickle.list_posts()
--- blogging/dao/post_dao_pickle.py
+++ blogging/dao/post_dao_pickle.py
@@ -1,24 +1,75 @@
+import pickle
 from blogging.dao.post_dao import PostDAO
+from blogging.post import Post
 
-class PostDAOPPickle(PostDAO):
-    def __init__(self):
-        # post collection here, I am pretty sure.
-        pass
-    
-    def search_post(self, key):
-        pass
+class PostDAOPickle(PostDAO):
+	def __init__(self, blog):
+		self.posts = []
 
-    def create_post(self, post):
-        pass
+		# TODO: Add code for loading posts from blog
+		# I think this is 3.3 in the PDF?
 
-    def retrieve_posts(self, search_string):
-        pass
+		# This import is in the __init__ method to avoid circular imports. There might be a better solution.
+		from blogging.blog import Blog
+		self.blog = blog # we pass "self" in blog.py, which is the Blog object itself here, as self.blog
 
-    def update_post(self, key, new_title, new_text):
-        pass
+	def search_post(self, code) -> Post:
+		for post in self.posts:
+			if post.code == code:
+				return post
 
-    def delete_post(self, key):
-        pass
+	def create_post(self, post) -> Post:
+		''' create a post in the blog '''
+		self.posts.append(post)
+		return post
 
-    def list_posts(self):
-        pass
+	def retrieve_posts(self, search_string) -> list[Post]:
+		''' retrieve posts in the blog that satisfy a search string '''
+		return [p for p in self.posts if search_string in p.title or search_string in p.text]
+
+	def update_post(self, code, new_title, new_text) -> bool:
+		''' update a post from the blog '''
+		updated_post = None
+
+		# first, search the post by code
+		for post in self.posts:
+			if post.code == code:
+				updated_post = post
+				break
+
+		# post does not exist
+		if not updated_post:
+			return False
+
+		# post exists, update fields and update timestamp
+		updated_post.update(new_title, new_text)
+		return True
+
+	def delete_post(self, code) -> bool:
+		''' delete a post from the blog '''
+		post_to_delete_index = -1
+
+		# first, search the post by code
+		for i in range(len(self.posts)):
+			if self.posts[i].code == code:
+				post_to_delete_index = i
+				break
+
+		# post does not exist
+		if post_to_delete_index == -1:
+			return False
+
+		# post exists, delete post
+		self.posts.pop(post_to_delete_index)
+		# NOTE: I think we should reserialize/resave here, but I am not sure.
+		return True
+
+	def list_posts(self) -> list[Post]:
+		''' list all posts from the blog from the 
+			more recently added to the least recently added'''
+
+		# list existing posts
+		posts_list = []
+		for i in range(-1, -len(self.posts)-1, -1):
+			posts_list.append(self.posts[i])
+		return posts_list
