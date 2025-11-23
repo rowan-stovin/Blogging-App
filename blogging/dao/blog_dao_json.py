2f5bb654c75e6e700458cce4b2ab1e87e67c2550
Elliot Simpson <elliotsimpson@uvic.ca>
2025-11-23 14:16:15 -0800

    Refactor blog_dao_json.py and bug fix: save blog functionality didn't check if autosave true

--- blogging/dao/blog_dao_json.py
+++ blogging/dao/blog_dao_json.py
@@ -1,62 +1,71 @@
-from blogging.dao.blog_dao import BlogDAO
-from blogging.blog import Blog
-from blogging.configuration import Configuration
-from blogging.dao.blog_decoder import BlogDecoder
-from blogging.dao.blog_encoder import BlogEncoder
 import json
+from blogging.blog import Blog
+from blogging.dao.blog_dao import BlogDAO
+from blogging.dao.blog_encoder import BlogEncoder
+from blogging.dao.blog_decoder import BlogDecoder
+from blogging.configuration import Configuration
+
 
 class BlogDAOJSON(BlogDAO):
 	def __init__(self):
 		self.blogs = {}
-		self.config = Configuration()
-		if self.config.autosave == True:
+		self.autosave = Configuration.autosave
+		self.filepath = Configuration.blogs_file
+		self.load_blogs()
+	
+	def load_blogs(self) -> None:
+		''' a helper method to load blogs '''
+		if self.autosave:
 			try:
-				with open(self.config.blogs_file, 'r') as file:
+				with open(self.filepath, 'r') as file:
 					line = file.readline().strip()
-					while line != '':
+
+					while line:
 						blog = json.loads(line, cls=BlogDecoder)
 						self.blogs[blog.id] = blog
 						line = file.readline().strip()
+
+			except FileNotFoundError:
+				return None
 			except:
-				self.blogs ={}
-		
+				raise RuntimeError("load_blogs failed!")
 	
-	def search_blog(self, key):
+	def save_blogs(self) -> None:
+		''' a helper method to save blogs '''
+		if self.autosave:
+			with open(self.filepath, 'w') as file:
+				for blog in self.blogs.values():
+					file.write(f"{json.dumps(blog, cls=BlogEncoder)}\n")
+
+	def search_blog(self, key) -> Blog:
+		''''''
 		return self.blogs.get(key)
 
-	def create_blog(self, blog):
+	def create_blog(self, blog) -> Blog:
+		''''''
 		# add a new blog
 		self.blogs[blog.id] = blog
-		with open(self.config.blogs_file, 'w') as file:
-			for each in self.blogs.values():
-				file.write(f"{json.dumps(each, cls=BlogEncoder)}\n")
+		self.save_blogs()
 		return blog
 
-	def retrieve_blogs(self, search_string):
-		retrieved_blogs = []
-		for blog in self.blogs.values():
-			if search_string in blog.name:
-				retrieved_blogs.append(blog)
-		return retrieved_blogs
+	def retrieve_blogs(self, search_string) -> list[Blog]:
+		''''''
+		return [b for b in self.blogs.values() if search_string in b.name]
 
-	def update_blog(self, key, blog):
+	def update_blog(self, key, blog) -> bool:
+		''''''
 		self.blogs.pop(blog.id)
 		blog.id = key
 		self.blogs[key] = blog
-		with open(self.config.blogs_file, 'w') as file:
-			for each in self.blogs.values():
-				file.write(f"{json.dumps(each, cls=BlogEncoder)}\n")
+		self.save_blogs()
 		return True
 
-	def delete_blog(self, key):
+	def delete_blog(self, key) -> bool:
+		''''''
 		self.blogs.pop(key)
-		with open(self.config.blogs_file, 'w') as file:
-			for each in self.blogs.values():
-				file.write(f"{json.dumps(each, cls=BlogEncoder)}\n")
+		self.save_blogs()
 		return True
 
-	def list_blogs(self):
-		blogs_list = []
-		for blog in self.blogs.values():
-			blogs_list.append(blog)
-		return blogs_list
+	def list_blogs(self) -> list[Blog]:
+		''''''
+		return [b for b in self.blogs.values()]
