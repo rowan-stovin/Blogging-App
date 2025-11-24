e9d58f9941a3f0ef4f0d28cbf4a9bfc19abd1260
rowab7 <rowanstovin@uvic.ca>
2025-11-23 21:11:10 -0800

    added some docstrings to blog_dao_json

--- blogging/dao/blog_dao_json.py
+++ blogging/dao/blog_dao_json.py
@@ -8,13 +8,14 @@ from blogging.configuration import Configuration
 
 class BlogDAOJSON(BlogDAO):
 	def __init__(self):
+		''' Constructs a BlogDAOJSON class.'''
 		self.blogs = {}
 		self.autosave = Configuration.autosave
 		self.filepath = Configuration.blogs_file
 		self.load_blogs()
 	
 	def load_blogs(self) -> None:
-		''' a helper method to load blogs '''
+		''' a helper method to load blogs.'''
 		if self.autosave:
 			try:
 				with open(self.filepath, 'r') as file:
@@ -31,29 +32,28 @@ class BlogDAOJSON(BlogDAO):
 				raise RuntimeError("load_blogs failed!")
 	
 	def save_blogs(self) -> None:
-		''' a helper method to save blogs '''
+		''' a helper method to save blogs.'''
 		if self.autosave:
 			with open(self.filepath, 'w') as file:
 				for blog in self.blogs.values():
 					file.write(f"{json.dumps(blog, cls=BlogEncoder)}\n")
 
 	def search_blog(self, key) -> Blog:
-		''''''
+		''' Searches for a blog by a key.'''
 		return self.blogs.get(key)
 
 	def create_blog(self, blog) -> Blog:
-		''''''
-		# add a new blog
+		''' Creates a new blog.'''
 		self.blogs[blog.id] = blog
 		self.save_blogs()
 		return blog
 
 	def retrieve_blogs(self, search_string) -> list[Blog]:
-		''''''
+		''' Retrieves a list of blogs with the given string in its name.'''
 		return [b for b in self.blogs.values() if search_string in b.name]
 
 	def update_blog(self, key, blog) -> bool:
-		''''''
+		''' Updates a blog.'''
 		self.blogs.pop(blog.id)
 		blog.id = key
 		self.blogs[key] = blog
@@ -61,11 +61,11 @@ class BlogDAOJSON(BlogDAO):
 		return True
 
 	def delete_blog(self, key) -> bool:
-		''''''
+		''' Deletes the blog with the given key.'''
 		self.blogs.pop(key)
 		self.save_blogs()
 		return True
 
 	def list_blogs(self) -> list[Blog]:
-		''''''
+		''' Lists all blogs.'''
 		return [b for b in self.blogs.values()]
