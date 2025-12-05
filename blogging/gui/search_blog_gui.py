3a815124b01f6f58727abfcbea68235774fdcdd6
rowab7 <rowanstovin@gmail.com>
2025-12-04 23:12:00 -0800

    added some implementation of search blog

--- blogging/gui/search_blog_gui.py
+++ blogging/gui/search_blog_gui.py
@@ -13,3 +13,42 @@ class SearchBlogGUI(QMainWindow):
         self.controller = controller
         self.setWindowTitle("Search for a Blog")
         self.resize(600, 200)
+
+        search_layout = QHBoxLayout()
+        
+        blog_id_label = QLabel("Enter the Blog's ID")
+        blog_id_text = QLineEdit()
+        
+        info_layout = QGridLayout()
+
+        #blog_label = QLabel("Blog")
+        blog_id_label = QLabel("Enter the Blog's ID")
+        blog_id_text = QLineEdit()
+        blog_name_label = QLabel("Enter the Blog's name")
+        blog_name_text = QLineEdit()
+        blog_url_label = QLabel("Enter the Blog's URL")
+        blog_url_text = QLineEdit()
+        blog_email_label = QLabel("Enter the Blog's Email")
+        blog_email_text = QLineEdit()
+
+        #info_layout.addWidget(blog_label)
+        info_layout.addWidget(blog_id_label, 0, 0)
+        info_layout.addWidget(blog_id_text, 0, 1)
+        info_layout.addWidget(blog_name_label, 1, 0)
+        info_layout.addWidget(blog_name_text, 1, 1)
+        info_layout.addWidget(blog_url_label, 2, 0)
+        info_layout.addWidget(blog_url_text, 2, 1)
+        info_layout.addWidget(blog_email_label, 3, 0)
+        info_layout.addWidget(blog_email_text, 3, 1)
+
+        layout = QVBoxLayout()
+        
+        top_widget = QWidget()
+        top_widget.setLayout(search_layout) 
+        bottom_widget = QWidget()
+        bottom_widget.setLayout(info_layout)
+        layout.addWidget(top_widget)
+        layout.addWidget(bottom_widget)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
